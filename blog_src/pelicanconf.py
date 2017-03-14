463029c7cee103d215a5ecd74a95111be0bf2e3b
Al Sweigart <asweigart@gmail.com>
2017-03-14 10:27:32 -0700

    Adding pelican blog which replaces the old wordpress blog

---
+++ blog_src/pelicanconf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Al Sweigart'
+SITENAME = 'The Invent with Python Blog'
+SITEURL = 'http://localhost:8000/blog'
+SITEURL = 'https://inventwithpython.com/blog'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/AlSweigart'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+STATIC_PATHS = ['blogstatic']
+LOAD_CONTENT_CACHE = False
+
+THEME = "voidy-bootstrap"
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+FILENAME_METADATA = '(?P<slug>.*?)\.html'
+
+SKIP_COLOPHON = True
+
+
+OUTPUT_PATH = 'blog'
+DISQUS_SITENAME = 'inventwithpython'
+GOOGLE_ANALYTICS = 'UA-5459430-3'
