b43a4ec79b13b7097312c38f406a10976c4899de
Al Sweigart <asweigart@gmail.com>
2021-09-14 12:33:07 -0500

    Cleaning up the website, fixing links.

--- blog_src/pelicanconf.py
+++ blog_src/pelicanconf.py
@@ -11,7 +11,7 @@ PATH = 'content'
 
 TIMEZONE = 'America/Los_Angeles'
 
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
