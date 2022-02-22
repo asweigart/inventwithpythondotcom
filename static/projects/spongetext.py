bea9485098db881d56b75a11c2f6f0af65ca0eb0
Al Sweigart <asweigart@gmail.com>
2022-02-21 23:31:53 -0600

    Adding webp and various other files.

---
+++ static/projects/spongetext.py
@@ -0,0 +1,53 @@
+"""sPoNgEcAsE, by Al Sweigart al@inventwithpython.com
+Translates English messages into sPOnGEtExT.
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: tiny, beginner, word"""
+
+import random
+
+try:
+    import pyperclip  # pyperclip copies text to the clipboard.
+except ImportError:
+    pass  # If pyperclip is not installed, do nothing. It's no big deal.
+
+
+def main():
+    """Run the Spongetext program."""
+    print('''sPoNgEcAsE, bY aL sWeIGaRt Al@iNvEnTwItHpYtHoN.cOm
+
+eNtEr YoUr MeSsAgE:''')
+    spongetext = englishToSpongecase(input('> '))
+    print()
+    print(spongetext)
+
+    try:
+        pyperclip.copy(spongetext)
+        print('(cOpIed SpOnGeTexT to ClIpbOaRd.)')
+    except:
+        pass  # Do nothing if pyperclip wasn't installed.
+
+
+def englishToSpongecase(message):
+    """Return the spongetext form of the given string."""
+    spongetext = ''
+    useUpper = False
+
+    for character in message:
+        if not character.isalpha():
+            spongetext += character
+            continue
+
+        if useUpper:
+            spongetext += character.upper()
+        else:
+            spongetext += character.lower()
+
+        # Flip the case, 90% of the time.
+        if random.randint(1, 100) <= 90:
+            useUpper = not useUpper  # Flip the case.
+    return spongetext
+
+
+# If this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
---
+++ static/sourceCodesRecursion.js
@@ -0,0 +1,2 @@
+var sourceCodes = {"abcTraceback": "def a():\r\n    print(\'Start of a()\')\r\n    b()\r\n\r\ndef b():\r\n    print(\'Start of b()\')\r\n    c()\r\n\r\ndef c():\r\n    print(\'Start of c()\')\r\n    42 \/ 0\r\n\r\na()\r\n",
+};
